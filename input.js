e6fda9e456f8bdc4b1daf8c51d2d981f8eeb2a29
Anne Franklin <anmerfrank@gmail.com>
2019-09-25 22:40:05 +0000

    Functioning snake game! Pair programming with Logan R.

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
 
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection; 
+
+// setup input and encoding:
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,10 +16,25 @@ const setupInput = function() {
 }
 
 
-const handleUserInput = function(data) {
+// handling user input:
+
+const handleUserInput = function() {
+  console.log(connection);
   process.stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
     }
   })
 };

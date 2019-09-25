b442c985388017adce484752f03faea9c7fc2eb1
Anne Franklin <anmerfrank@gmail.com>
2019-09-25 21:19:27 +0000

    Snake game

---
+++ client.js
@@ -0,0 +1,17 @@
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.88.254',
+    port: 50541
+  });
+ // interpret incoming data as text
+ 
+ conn.setEncoding('utf8');
+ 
+ conn.on('data', (data) => {
+     console.log("Server says: " + data);
+ 
+ return conn;
+ });
+ };
+
+ module.exports = { connect };

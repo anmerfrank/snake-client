1a5c1a62b7cde587e3dee820142e9fc46172d8b1
Anne Franklin <anmerfrank@gmail.com>
2019-09-25 21:46:58 +0000

    Snake game - pair programming with Logan

--- play.js
+++ play.js
@@ -4,6 +4,26 @@ const { connect } = require('./client');
 * Establishes connection with the game server
 */
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => {
+    handleUserInput(data) });
+  return stdin;
+}
 
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(data) {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+};
+
+setupInput();
+handleUserInput();

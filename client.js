fa8fb32a3a72312c6d87428a715806b3c631f141
Anne Franklin <anmerfrank@gmail.com>
2019-09-25 21:33:00 +0000

    Snake game - connecting to server

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+const net = require('net');
+
 const connect = function() {
   const conn = net.createConnection({
     host: '192.168.88.254',
@@ -6,12 +8,23 @@ const connect = function() {
  // interpret incoming data as text
  
  conn.setEncoding('utf8');
- 
+
+ conn.on('connect', () => {
+   console.log('Successfully connected to game server!');
+   conn.write("Name: AMF");
+  //  conn.write("Move: up");
+  //  conn.write("Move: down");
+  //  conn.write("Move: right");
+  //  conn.write("Move: left");
+ }); 
+
  conn.on('data', (data) => {
-     console.log("Server says: " + data);
+     console.log("Server says: ", data);
  
+conn.setEncoding('utf8');
+
  return conn;
  });
  };
 
- module.exports = { connect };
+module.exports = { connect };
